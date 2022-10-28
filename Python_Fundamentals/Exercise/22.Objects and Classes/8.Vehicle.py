bc363afbf831cc83ba83db14ef29e835b67a412b
Lachezar Indarov <71582773+LachezarIndarov@users.noreply.github.com>
2022-10-28 07:38:53 +0300

    Add files via upload

---
+++ Python_Fundamentals/Exercise/22.Objects and Classes/8.Vehicle.py
@@ -0,0 +1,53 @@
+class Vehicle:
+
+    def __init__(self,type,model,price):
+        self.type = type
+        self.model = model
+        self.price = price
+        self.owner = None
+
+
+    def buy(self, money: int, owner: str):
+        if money >= self.price and self.owner == None:
+            self.owner = owner
+            change = money - self.price
+            return f"Successfully bought a {self.type}. Change: {change:.2f}"
+        elif money < self.price:
+            return "Sorry, not enough money"
+        elif money >= self.price and self.owner != None:
+            return "Car already sold"
+
+
+
+    def sell(self):
+        if self.owner != None:
+           self.owner = None
+        else:
+           return "Vehicle has no owner"
+
+    def __repr__(self):
+        if self.owner != None:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        else:
+            return f"{self.model} {self.type} is on sale: {self.price}"
+
+# input
+vehicle_type = "car"
+model = "BMW"
+price = 30000
+vehicle = Vehicle(vehicle_type, model, price)
+print(vehicle.buy(15000, "Peter"))
+print(vehicle.buy(35000, "George"))
+print(vehicle)
+vehicle.sell()
+print(vehicle)
+
+# output
+"""
+Sorry, not enough money
+Successfully bought a car. Change: 5000.00
+BMW car is owned by: George
+BMW car is on sale: 30000
+
+"""
+
