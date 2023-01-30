7987c2211d117520cf02b1d664c3d8c16e05117b
Lachezar Indarov <71582773+LachezarIndarov@users.noreply.github.com>
2023-01-30 11:29:17 +0200

    Add files via upload

---
+++ Python Advanced/Exercise/04.Lists as Stack and Queues/06.Balanced Parentheses.py
@@ -0,0 +1,45 @@
+"""
+input1
+{[()]}
+
+output1
+YES
+
+input2
+{[(])}
+
+output2
+NO
+
+input3
+{{[[(())]]}}
+
+output3
+YES
+
+"""
+
+expression = input()
+
+opening_brackets = []
+
+pairs = { '(': ')', '[': ']', '{': '}' }
+
+balanced = True
+for ch in expression:
+    if ch in '([{':
+        opening_brackets.append(ch)
+    elif not opening_brackets:
+        balanced = False
+    else:
+        last_opening_bracket = opening_brackets.pop()
+        if pairs[last_opening_bracket] != ch:
+            balanced = False
+
+    if not balanced:
+        break
+if not balanced or opening_brackets:
+    print("NO")
+else:
+    print("YES")
+
