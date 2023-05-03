2a829f4ad98472b31178d4c5f54247d771ba3622
Lachezar Indarov <71582773+LachezarIndarov@users.noreply.github.com>
2023-05-03 10:15:39 +0300

    Add files via upload

---
+++ Python Advanced/Lab/15.File Handling/Training/with.py
@@ -0,0 +1,7 @@
+file1 = open('C:/Users/indarov/PycharmProjects/Exercise 1/Python Advanced/Lab/15.File Handling/Training/demo', 'w')
+
+with open('C:/Users/indarov/PycharmProjects/Exercise 1/Python Advanced/Lab/15.File Handling/Training/demo', 'w') as file2:
+    # use file2
+    pass
+
+# cannot use file2
