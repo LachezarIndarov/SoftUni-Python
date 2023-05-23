e381328a58151e6d04974bf58b5102bfe752e576
Lachezar Indarov <71582773+LachezarIndarov@users.noreply.github.com>
2023-05-23 14:32:05 +0300

    Add files via upload

---
+++ Python Advanced/Lab/19.Modules/builtin_modules_demos.py
@@ -0,0 +1,14 @@
+from os import listdir
+from math import pi
+from random import shuffle as random_shuffle
+
+
+def shuffle(values):
+    pass
+
+# listdir - Принтира файловете в директорията, която сме подали
+print(
+    listdir('./')
+)
+
+print(pi)
