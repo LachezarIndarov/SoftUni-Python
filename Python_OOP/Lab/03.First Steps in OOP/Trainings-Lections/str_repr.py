a78508a46bf9ebd97b4637be60a867f0ffe3e9bb
Lachezar Indarov <71582773+LachezarIndarov@users.noreply.github.com>
2023-08-03 09:01:56 +0300

    Add files via upload

---
+++ Python_OOP/Lab/03.First Steps in OOP/Trainings-Lections/str_repr.py
@@ -0,0 +1,9 @@
+values = [
+    # [1, 2, 3, 4, 5],  # list
+    # {1, 2, 3, 4, 5},  # set
+    # {'a': 1, 'b': 2}, # dictionary
+    'Text',
+]
+
+[print(str(x)) for x in values]
+[print(repr(x)) for x in values]
