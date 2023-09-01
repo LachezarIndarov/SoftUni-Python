e1b04e8ea1e7e986afc387362bd40fe4b5470b94
Lachezar Indarov <71582773+LachezarIndarov@users.noreply.github.com>
2023-09-01 09:27:01 +0300

    Add files via upload

---
+++ Python_OOP/Exercise/6.Classes and Objects/6.Guild System.py
@@ -0,0 +1,29 @@
+from project.guilt import Guild
+from project.player import Player
+
+player = Player("George", 50, 100)
+print(player.add_skill("Shield Break", 20))
+print(player.player_info())
+guild = Guild("UGT")
+print(guild.assign_player(player))
+print(guild.guild_info())
+
+"""
+output:
+Skill Shield Break added to the collection of the player George
+Name: George
+Guild: Unaffiliated
+HP: 50
+MP: 100
+===Shield Break - 20
+
+Welcome player George to the guild UGT
+Guild: UGT
+Name: George
+Guild: UGT
+HP: 50
+MP: 100
+===Shield Break - 20
+
+
+"""
