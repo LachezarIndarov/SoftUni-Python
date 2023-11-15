ce923a4c1cedab386e15f46ab7d27e7b370c0277
Lachezar Indarov <71582773+LachezarIndarov@users.noreply.github.com>
2023-11-15 08:56:01 +0200

    Add files via upload

---
+++ Python_OOP/Exercise/10.Encapsulation/1.Wild Cat Zoo/project/cheetah.py
@@ -0,0 +1,7 @@
+from project.animal import Animal
+
+class Cheetah(Animal):
+    Money_FOR_CARE = 60
+
+    def __init__(self, name, gender, age):
+        super().__init__(name, gender, age, self.Money_FOR_CARE)
